96afe85315456b984e21f43925f2406500ec3694
Priyanshu Mohapatra <119274090+PriyanshuMohapatra@users.noreply.github.com>
2024-04-12 10:56:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,19 +4,13 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
 
-// Redirect to work2.html
-app.get('/work2.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'work2.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname,'proj2.html'));
 });
-
-
-app.get('/cake', (req, res) => {
-    res.sendFile(__dirname+'/proj2.html');
-  });
-
+app.get('/work2.html', (req, res) => {
+  res.sendFile(path.join(__dirname,'work2.html'));
+});
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
